e39fdde3d190699c0b219926ab54ab494337ad6b
MohamedSonata <sonata.1993@yahoo.com>
2026-03-23 21:31:35 +0200

    first commit

--- config/database.ts
+++ config/database.ts
@@ -40,8 +40,28 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database 
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
+        statement_timeout: env.int('DATABASE_STATEMENT_TIMEOUT', 30000),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: {
+        min: env.int('DATABASE_POOL_MIN', 2),
+        max: env.int('DATABASE_POOL_MAX', 5),
+        acquireTimeoutMillis: env.int('DATABASE_ACQUIRE_TIMEOUT', 60000),
+        createTimeoutMillis: env.int('DATABASE_CREATE_TIMEOUT', 30000),
+        idleTimeoutMillis: env.int('DATABASE_IDLE_TIMEOUT', 30000),
+        reapIntervalMillis: env.int('DATABASE_REAP_INTERVAL', 1000),
+        createRetryIntervalMillis: 200,
+        propagateCreateError: false,
+        afterCreate: (conn: any, done: Function) => {
+          conn.query('SET tcp_keepalives_idle TO 60;', (err: Error) => {
+            if (err) {
+              console.error('Error setting keepalive: ', err);
+            }
+            done(err, conn);
+          });
+        },
+      },
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      asyncStackTraces: env.bool('DATABASE_ASYNC_STACK_TRACES', false),
     },
     sqlite: {
       connection: {
